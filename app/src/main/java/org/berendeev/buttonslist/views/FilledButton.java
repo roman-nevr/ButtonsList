44844ab10f724ce364c952dff9096c32d9c8b2de
roman-nevr <roman-nevr@yandex.ru>
2017-03-01 17:53:20 +0300

    initial commit

---
+++ app/src/main/java/org/berendeev/buttonslist/views/FilledButton.java
@@ -0,0 +1,20 @@
+package org.berendeev.buttonslist.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+
+public class FilledButton extends Button {
+    public FilledButton(Context context) {
+        super(context);
+    }
+
+    public FilledButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FilledButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
